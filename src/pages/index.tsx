3bfe3ac5fe9a73a54aa6545551a83ee48cfb6df1
makotot <makoto.ttn@gmail.com>
2021-08-15 01:10:46 +0900

    add pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react'
-import {
-  Box,
-  Flex,
-  Heading,
-  Paragraph,
-  Grid,
-  Message,
-} from 'theme-ui'
+import { Box, Flex, Heading, Paragraph, Grid, Message } from 'theme-ui'
 import { BoxLink } from '../components/BoxLink'
 
 function App() {
@@ -26,6 +19,7 @@ function App() {
           <Grid gap="3" columns={[null, '1fr 1fr 1fr']}>
             <BoxLink href="/Scrollspy">Scrollspy</BoxLink>
             <BoxLink href="/ReadingProgress">Reading Progress</BoxLink>
+            <BoxLink href="/Pagination">Pagination</BoxLink>
           </Grid>
         </Box>
       </Box>
